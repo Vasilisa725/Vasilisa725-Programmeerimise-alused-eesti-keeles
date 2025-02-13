deb6dec810923d260d3c88ca9bc1fd5c162f4f3e
Vasilisa725 <vasilisavasilenko719@gmail.com>
2025-02-13 15:20:21 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+import emsine as esimenefunc
+import teine as teineFail
+import kolmas as kolmasfunc
+
+userInput = input("Milline ülesanne sa tahad ülevaadata")
+if userInput == "1":
+    emsineFail.myFunc()
+elif userInput == "2":
+    teineFail.myPykkar()
+else:
+    pass
+
+Š
---
+++ start.py
@@ -0,0 +1,3 @@
+def myFunc():
+    print("Test")
+    
