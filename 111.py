4092ebc650c12c3fef8a1d630e8cf67c51bcc91b
Vasilisa725 <vasilisavasilenko719@gmail.com>
2025-05-07 11:03:53 +0300

    Update 111.py

--- 111.py
+++ 111.py
@@ -4,33 +4,58 @@ import time
 import csv
 import datetime
 
+#1) salvestada tulemused failisse  ping_log.csv
+# Time,status
+# 05.04.2025,OK
+# 05.04.2025,Fail
+# hosts= ["8.8.8.8","1.1.1.1", "192.168.56.1" ]
+# 
+# with open("ping_log.csv","w",newline="") as file:
+#     writer = csv.writer(file)
+#     writer.writerow(["Time","Status"])
+# 
+# while True:
+#     print("kättesadavuse kontroll")
+#     now = datetime.datetime.now()
+#     result = ""
+#     for elem in hosts:
+#         response = os.system(f"ping -n 1 {elem} > null")
+#         if response == 0:
+#             result = "OK"
+#             print(elem, "kätesadavalt")
+#         else:
+#             result = "Fail"
+#             print(elem, "ei ole kättesadavalt")
+#         
+#         with open("ping_log.csv","a",newline="") as file:
+#             writer = csv.writer(file)
+#             writer.writerow([now,result])
+# 
+# 
+#         print("-"*30)
+#         time.sleep(5)
+#         
+# võimalus vaadata kõik protsessid arvutiss
+# salvestada need failisse tasklist ["Time", "Proccess name", "Memory usage"]
+import os
 
-hosts = ["8.8.8.8","1.1.1.1", "192.168.1.1" ]
-
-with open("ping_loh.csv","w",newline="") as file:
+with open("taskid.csv","w",newline="") as file:
     writer = csv.writer(file)
-    writer.writerow(["Time","Status"])
-    
-while True:
-    print("kättesadavuse kontroll")
+    writer.writerow(["Time","Proccess name","Memory Usage"])
+
+# Running the aforementioned command and saving its output
+output = os.popen("tasklist").read()
+test = output.splitlines()
+for i in range(7,len(test)):
     now = datetime.datetime.now()
-    result = "OK"
-    for elem in hosts:
-        response = os.system(f"ping -n 1 {elem} > null")
-        if response == 0:
-            result = "OK"
-            print(elem, "kättesadavalt")
-        else:
-            result = "Fail"
-            print(elem, "ei ole kättesadavalt")
-            
-            
-        with open("ping_loh.csv","w",newline="") as file:
-            writer = csv.writer(file)
-            writer.writerow([now,result])
-            
-        print("-"*30)
-        time.sleep(5)
-        
-        
-        
+    proccesName = test[i].split()
+    name = proccesName[0]
+    memory = proccesName[-2]
+    
+    with open("taskid.csv","a",newline="") as file:
+        writer = csv.writer(file)
+        writer.writerow([now,name,memory])
+
+    print("time: ",now, "name: ", name,"memory: ", memory )
+    
+# Displaying the output
