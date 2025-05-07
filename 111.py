e8963e20f23aada5642add294f64a1ac669af904
Vasilisa725 <vasilisavasilenko719@gmail.com>
2025-05-07 10:31:10 +0300

    Update 111.py

--- 111.py
+++ 111.py
@@ -1,17 +1,36 @@
 import os
 import time
 
+import csv
+import datetime
+
+
 hosts = ["8.8.8.8","1.1.1.1", "192.168.1.1" ]
 
+with open("ping_loh.csv","w",newline="") as file:
+    writer = csv.writer(file)
+    writer.writerow(["Time","Status"])
+    
 while True:
     print("kättesadavuse kontroll")
+    now = datetime.datetime.now()
+    result = "OK"
     for elem in hosts:
         response = os.system(f"ping -n 1 {elem} > null")
         if response == 0:
+            result = "OK"
             print(elem, "kättesadavalt")
         else:
+            result = "Fail"
             print(elem, "ei ole kättesadavalt")
+            
+            
+        with open("ping_loh.csv","w",newline="") as file:
+            writer = csv.writer(file)
+            writer.writerow([now,result])
             
         print("-"*30)
         time.sleep(5)
+        
+        
         
