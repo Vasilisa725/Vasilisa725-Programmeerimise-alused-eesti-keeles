bea1fa753cad1b2e37a8f83ece3767f5f476a13f
Vasilisa725 <vasilisavasilenko719@gmail.com>
2025-05-07 10:30:25 +0300

    Add files via upload

---
+++ 111.py
@@ -0,0 +1,17 @@
+import os
+import time
+
+hosts = ["8.8.8.8","1.1.1.1", "192.168.1.1" ]
+
+while True:
+    print("kättesadavuse kontroll")
+    for elem in hosts:
+        response = os.system(f"ping -n 1 {elem} > null")
+        if response == 0:
+            print(elem, "kättesadavalt")
+        else:
+            print(elem, "ei ole kättesadavalt")
+            
+        print("-"*30)
+        time.sleep(5)
+        
